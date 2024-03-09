a5533bba7cc24ce7a5ab8db331dbeb7d44c4681a
Deepanshu8821 <deepanshu@alteredsecurity.com>
2024-03-09 16:03:42 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is Ci/Cd Pipeline....</h1>
+        <h2>This is latest Ci/Cd Pipeline....</h2>
       </header>
     </div>
   );
